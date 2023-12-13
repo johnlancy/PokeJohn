4028ac09bb215de233bfb95d36bdd7739210b4f7
johnlancy <johnlancy98@gmail.com>
2023-12-13 17:36:29 -0500

    updated header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import localFont from '@next/font/local';
+import Link from 'next/link';
 
 const pokefont = localFont({
 	src: '../../public/fonts/Pokemon Solid.ttf',
@@ -10,7 +11,21 @@ export default function Header() {
 		<header
 			className={`${pokefont.className} bg-green-600/70 w-full p-5 backdrop-blur-md position: fixed flex justify-center text-2xl tracking-widest`}
 		>
-			<p className='header-text-stroke text-4xl'>PokeJohn</p>
+			<nav className='flex w-full max-w-7xl mx-auto'>
+				<Link
+					href={'/'}
+					className='header-text-stroke text-4xl hover:scale-110 transition-all duration-300 ease-in-out'
+				>
+					PokeJohn
+				</Link>
+				<div className='flex-1' />
+				<Link
+					className='header-text-stroke text-3xl hover:scale-110 transition-all duration-300 ease-in-out'
+					href={'/counter'}
+				>
+					Counter
+				</Link>
+			</nav>
 		</header>
 	);
 }
